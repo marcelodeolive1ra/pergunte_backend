caf25429140db6a3d5db40767cfa8463f256b7f5
marcelodeolive1ra <marcelodeoliveira@outlook.com>
2017-01-19 22:53:32 -0200

    Corrige ordenação da lista de matérias

--- pergunte/views.py
+++ pergunte/views.py
@@ -164,11 +164,11 @@ def getMateriasPorStatus(request, statusMateria):
             if tipoUsuario == 'aluno':
                 aluno = Aluno.objects.get(email=email)
                 materias_encontradas = aluno.materias.filter(materiaAtiva=statusMateria). \
-                    order_by("nomeDisciplina").order_by("-semestre").order_by("-ano")
+                    order_by('-ano', '-semestre', 'nomeDisciplina')
             else:
                 professor = Professor.objects.get(email=email)
-                materias_encontradas = Materia.objects.filter(professor=professor).filter(materiaAtiva=statusMateria).\
-                    order_by("nomeDisciplina").order_by("-semestre").order_by("-ano")
+                materias_encontradas = Materia.objects.filter(professor=professor).filter(materiaAtiva=statusMateria). \
+                    order_by('-ano', '-semestre', 'nomeDisciplina')
 
             for materia in materias_encontradas:
                 materias.append(dicionario_materia(materia))
